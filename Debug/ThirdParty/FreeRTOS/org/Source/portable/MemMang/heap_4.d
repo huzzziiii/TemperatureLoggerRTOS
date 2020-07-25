fb5ded7ec373c5786dea5506d191041fc03acd37
Huzaifa Asif <huzaifaasif@HUZZi-mac.local>
2020-07-25 12:14:35 -0400

    Integrated FreeRTOS - two threads running

---
+++ Debug/ThirdParty/FreeRTOS/org/Source/portable/MemMang/heap_4.d
@@ -0,0 +1,29 @@
+ThirdParty/FreeRTOS/org/Source/portable/MemMang/heap_4.o: \
+ ../ThirdParty/FreeRTOS/org/Source/portable/MemMang/heap_4.c \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/FreeRTOS.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/Config/FreeRTOSConfig.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/projdefs.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/portable.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/task.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/list.h
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/Config/FreeRTOSConfig.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/projdefs.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/portable.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/task.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/list.h:
---
+++ Debug/ThirdParty/FreeRTOS/org/Source/tasks.d
@@ -0,0 +1,38 @@
+ThirdParty/FreeRTOS/org/Source/tasks.o: \
+ ../ThirdParty/FreeRTOS/org/Source/tasks.c \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/FreeRTOS.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/Config/FreeRTOSConfig.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/projdefs.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/portable.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/task.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/list.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/timers.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/task.h \
+ /Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/stack_macros.h
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/Config/FreeRTOSConfig.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/projdefs.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/portable.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/task.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/list.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/timers.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/task.h:
+
+/Users/huzaifaasif/STM32CubeIDE/workspace_1.0.2/TemperatureLoggerRTOS/ThirdParty/FreeRTOS/org/Source/include/stack_macros.h:
